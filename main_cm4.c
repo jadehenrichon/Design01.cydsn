2bd124c8644541bc41211246b60e33083b80cb04
jadehenrichon <jadehenrichon@hotmail.com>
2021-03-10 16:48:57 -0500

    partie1

--- main_cm4.c
+++ main_cm4.c
@@ -16,11 +16,28 @@
 #include "params.h"
 #include "queue.h"
 
+
+void vTaskInversionLED(){
+    for(;;){
+
+if(Cy_GPIO_Read(P1_1_PORT, P1_1_NUM)==0UL){
+Cy_GPIO_Write(P1_1_PORT, P1_1_NUM, 1UL);
+}
+else {
+Cy_GPIO_Write(P1_1_PORT, P1_1_NUM, 0UL);
+}
+vTaskDelay(pdMS_TO_TICKS(500));
+}
+}
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+TaskHandle_t xHandle = NULL;
+xTaskCreate(vTaskInversionLED,"Inversion",80, NULL, 3,xHandle);
+vTaskStartScheduler();
+    
 
     for(;;)
     {
